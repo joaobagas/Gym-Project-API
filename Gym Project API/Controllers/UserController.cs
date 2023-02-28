892bc48d454a0509426e512f43401729be21f122
Bernardo Dias <55239586+joaobagas@users.noreply.github.com>
2023-02-28 09:17:39 +0000

    Initial commit

---
+++ Gym Project API/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Gym_Project_API.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gym_Project_API.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        private DatabaseContext _dbContext;
+
+        public UserController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _dbContext = new DatabaseContext();
+        }
+
+        //[HttpPost("login")]
+        //public async Task<ActionResult<string>> Login([FromBody] UserDTO user)
+        //{
+        //    try
+        //    {
+        //        var dbPassword = _dbContext.UserPasswords.FirstOrDefault(p => p.Username == user.Username);
+        //        if (dbPassword == null) return BadRequest("Username not found!");
+        //        string hash = Authentication.GenerateHash(user.Password, dbPassword.Salt);
+        //        if (hash.Equals(dbPassword.Hash)) return Ok(Authentication.CreateToken(user));
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return BadRequest("Something went wrong!");
+        //    }
+        //    return BadRequest("Wrong password!");
+        //}
+    }
+}
---
+++ Gym Project API/DataAccess/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Gym_Project_API.DataAccess
+{
+    public class DatabaseContext : DBContext
+    {
+        // public virtual DbSet<UserDetails> UserDetails { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source = ./BPR-DB.db");
+        }
+    }
+}
